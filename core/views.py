1dd48c7ec2fcb5fd1cedb33225d1cbf1540a2e43
japhet Omorodion <japhet.omorodion@gmail.com>
2018-12-21 23:18:49 +0100

    added MovieListView, app url.py, and Template file(movie_list.html)

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from core.models import Movie
+from django.views.generic import ListView
 # Create your views here.
+
+class MovieListView(ListView):
+    model = Movie
+    
