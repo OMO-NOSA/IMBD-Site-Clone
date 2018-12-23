550a73804bf6b73e4f2f592c3e4ae360b932f267
japhet Omorodion <japhet.omorodion@gmail.com>
2018-12-23 16:20:46 +0100

    Added project Templates folder(Altered settings.py), core app template file(movie_detail and pagination to movie_list.html.)

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('movies', views.MovieList.as_view(), name='MovieList'),
+    path('movies', views.MovieListView.as_view(), name='MovieList'), 
+    path('movie/<int:pk>', views.MovieDetailView.as_view(), name='MovieDetail'),
+
+
+
+
+
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from core.models import Movie
-from django.views.generic import ListView
-# Create your views here.
+from django.views.generic import (DetailView,
+                                             ListView)
+# Create your views here
 
 class MovieListView(ListView):
     model = Movie
     
+class MovieDetailView(DetailView):
+    model = Movie
