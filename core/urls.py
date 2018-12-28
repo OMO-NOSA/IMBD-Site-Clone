ff7e179a0d1c46737b76d50a8aadd8879bb535e2
japhet Omorodion <japhet.omorodion@gmail.com>
2018-12-29 00:15:31 +0100

    Updated core/url.py

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('movies', views.MovieListView.as_view(), name='MovieList'), 
     path('movie/<int:pk>', views.MovieDetailView.as_view(), name='MovieDetail'),
     path('person', views.PersonDetailView.as_view(), name='PersonDetail'),
+    path('movie/<int:movie_id>/vote', views.CreateVote.as_view(), name='CreateVote'),
+    path('movie/<int:movie_id>/vote/<int:pk>',views.UpdateVote.as_view(), name='UpdateVote'),
 
 ]
