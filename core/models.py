101ebeb38a6a1de243bd7bc8a16aee7beb6d607c
japhet Omorodion <japhet.omorodion@gmail.com>
2019-01-06 21:32:21 +0100

    MoveImage model added for managing image uploads

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from uuid import uuid4
 
 # Create your models here.
 #MovieManager Class to enable prefetching of data from the DataBase.
@@ -166,3 +167,19 @@ class Vote(models.Model):
     objects = VoteManager()
     class Meta:
         unique_together = ('user', 'movie')
+
+
+
+def movie_directory_path_with_uuid(instance,filename):
+    return '{}/{}'.format(
+        instance.movie_id,uuid4()
+    )
+
+
+class MovieImage(models.Model):
+    image = models.ImageField(
+        upload_to=movie_directory_path_with_uuid)
+    uploaded = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
